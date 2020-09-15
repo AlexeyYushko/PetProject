a43013e7c7811cd879bd40f5c148b7d41df139c9
Alex <lehajusko@gmail.com>
2020-09-15 22:49:11 +0700

    Added new build&run scenarios into dev.ps1

--- dev.ps1
+++ dev.ps1
@@ -1,13 +1,45 @@
 $arg0= $args[0]
+$arg1= $args[1]
+
+if ($arg0 -eq 'b')
+{
+    if ($arg1 -eq 'dev')
+    {
+        docker-compose -f docker-compose.dev.yml build
+    }
+
+    if ($arg1 -eq 'prod')
+    {
+        docker-compose -f docker-compose.yml build
+    }
+}
 
 if ($arg0 -eq 'u')
 {
-    docker-compose up -d
+    if ($arg1 -eq 'dev')
+    {
+        docker-compose -f docker-compose.dev.yml up -d
+    }
+
+    if ($arg1 -eq 'prod')
+    {
+        docker-compose -f docker-compose.yml up -d
+    }
+
     docker run --rm --name angularapp -p 5300:4200 -v "${pwd}/NodeJs WebApp/angularapp:/var/www" -w "/var/www" nodejs
 }
 
 if ($arg0 -eq 'd')
 {
-    docker-compose down
+    if ($arg1 -eq 'dev')
+    {
+        docker-compose -f docker-compose.dev.yml down
+    }
+
+    if ($arg1 -eq 'prod')
+    {
+        docker-compose -f docker-compose.yml down
+    }
+
     docker stop $(docker ps -aq --no-trunc --filter name=^/angularapp$)
 }
