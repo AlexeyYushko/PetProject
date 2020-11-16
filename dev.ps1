50b0c9f518e02e3ee53af5199639043e5abef73e
Alex <lehajusko@gmail.com>
2020-11-16 20:10:56 +0700

    minor update to dev.ps1

--- dev.ps1
+++ dev.ps1
@@ -26,7 +26,8 @@ if ($arg0 -eq 'u')
         docker-compose -f docker-compose.yml up -d
     }
 
-    docker run --rm --name angularapp -p 5300:4200 -v "${pwd}/NodeJs WebApp/angularapp:/var/www" -w "/var/www" nodejs
+    #angularapp-nodejs - custom built image from dockerfile within nodejs directory
+    docker run --rm --name angularapp -p 5300:4200 -v "${pwd}/NodeJs WebApp/angularapp:/var/www" -w "/var/www" angularapp-nodejs
 }
 
 if ($arg0 -eq 'd')
